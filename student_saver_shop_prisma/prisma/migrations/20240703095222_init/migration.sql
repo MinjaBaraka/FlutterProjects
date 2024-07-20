f56d88662424e6e9e16e37cd4eafca049968dfe3
MinjaBaraka <45535420+MinjaBaraka@users.noreply.github.com>
2024-07-20 17:56:05 +0300

    20/07/2024
    FLUTTER PROJECTS

--- student_saver_shop_prisma/prisma/migrations/20240703095222_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `studentInstitution` on the `User` table. The data in that column could be lost. The data in that column will be cast from `TinyInt` to `VarChar(20)`.
-
-*/
--- AlterTable
-ALTER TABLE `User` MODIFY `studentInstitution` VARCHAR(20) NOT NULL;
